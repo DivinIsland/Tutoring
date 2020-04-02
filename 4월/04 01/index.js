5629e8cfceb874aa4dce2ddf1b2d360679c2119f
divinisland <choy0928@kakao.com>
2020-04-03 02:01:12 +0900

    0403

---
+++ 4월/04 01/index.js
@@ -0,0 +1,25 @@
+var cyg = {
+  firstName : `choe`,
+  lastName : `yulgyeom`,
+
+  body : {
+    height : 181,
+    weight : 110
+  }
+
+};
+
+
+var array = [1, "hello", [1,2,3], {hi : 100}];
+
+console.log(array[3].hi)
+
+var cyg_array = ['choe', 'yul', 'gyeom', 181, 110]
+
+console.clear();
+
+function addOne(x){
+  var y = x + 1;
+  return y;
+}
+
