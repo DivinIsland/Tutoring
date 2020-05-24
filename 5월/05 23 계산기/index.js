80de846bad8fa06b127763e7937f28ced86a2299
divinisland <choy0928@kakao.com>
2020-05-24 15:16:39 +0900

    0524

--- 5월/05 23 계산기/index.js
+++ 5월/05 23 계산기/index.js
@@ -7,24 +7,72 @@ const clearBtn = document.querySelector('#clear-btn')
 const numberArray = [0];
 const result = document.querySelector('#result');
 
+const calBtn = document.querySelector("#calBtn");
+
+const config={
+    numberList:[],
+    condition:""
+}
+
+function dataDrawing(config){
+    const { list,condition,target } = config;
+    const tmp = list.map(item=>item + condition).join("");
+    target.innerHTML = tmp ;
+}
+
+// inputNumber.addEventListener("keyup",function(e){
+//     result.textContent = +e.target.value
+// })
 
 plusBtn.addEventListener('click', () => {
-    numberArray.push(
-        numberArray[0] + +inputNumber.value
-    );
-    numberArray.shift();
+    // numberArray.push(
+    //     numberArray[0] + +inputNumber.value
+    // );
+    // numberArray.shift();
 
-    result.textContent = numberArray[0];    
+    // result.textContent = numberArray[0];
 
-})
+    config.numberList.push(+inputNumber.value);
+    config.condition = "+";
+    inputNumber.value = "";
+
+    const drawForamt ={
+        list:config.numberList,
+        condition:"+",
+        target:result
+    }
+    dataDrawing(drawForamt);
+
+});
+
+
+calBtn.addEventListener('click',function(e){
+    config.numberList.push(+inputNumber.value);
+    inputNumber.value ="";
+    let tmp;
+
+    if(config.condition === "+"){
+        tmp = config.numberList.reduce((x,y)=>x+y,0);
+    }
+    if(config.condition === "-"){
+        tmp = config.numberList[0] - config.numberList[1];
+    }
+    result.textContent = tmp;
+});
+
+
 
 minusBtn.addEventListener('click', () => {
-    numberArray.push(
-        numberArray[0] - +inputNumber.value
-    );
-    numberArray.shift();
+    // numberArray.push(
+    //     numberArray[0] - +inputNumber.value
+    // );
+    // numberArray.shift();
 
-    result.textContent = numberArray[0];    
+    // result.textContent = numberArray[0];    
+
+    config.numberList.push(+inputNumber.value);
+    config.condition = "-";
+    inputNumber.value = "";
 
 })
 
--- 5월/0523 끝말잇기/index.js
+++ 5월/0523 끝말잇기/index.js
@@ -2,20 +2,34 @@ const result = document.querySelector('#show-word')
 const inputWord = document.querySelector('#input-word');
 const inputBtn = document.querySelector('#word-btn')
 
-const wordArray = ['바나나'];
+const wordArray = [];
+
+
 
 
 
 inputBtn.addEventListener('click', () => {
+
+
+    if (wordArray.length === 0) {
+        wordArray.push(inputWord.value.trim());
+        result.textContent = wordArray;
+        inputWord.value = "";
+        return;
+    }
 
     const resultLength = wordArray[0].length;
     const inputLength = inputWord.value.length;
     const resultLast = wordArray[0].slice(resultLength - 1, resultLength);
     const inputFirst = inputWord.value.slice(0, 1);
 
+
+    
+        
+
     if (resultLast === inputFirst) {
         wordArray.pop();
-        wordArray.push(inputWord.value);
+        wordArray.push(inputWord.value.trim());
         result.textContent = wordArray;
         inputWord.value = "";
 
@@ -24,6 +38,8 @@ inputBtn.addEventListener('click', () => {
         inputWord.value = "";
     }
 
+    
+
 })
 
 
@@ -31,5 +47,8 @@ inputBtn.addEventListener('click', () => {
 
 
 
-
-
+/**
+ * 피드백 : 
+ * 1. 공백 제거 trim() 사용
+ * 2. 
+ */
